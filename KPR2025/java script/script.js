fc9b741d96bcae514f96f513b9b5b461d611f579
janarthan <janarthan672@gmail.com>
2025-06-06 16:27:13 +0530

    java

---
+++ KPR2025/java script/script.js
@@ -0,0 +1,25 @@
+// odd or even
+var number =10;
+if(number%2==0){
+    console.log("even")
+}
+var arr=[10,20,30]
+var arr2=[...arr,40,50,60]
+console.log(arr)
+console.log(arr2)
+var mark=[90,80,70,60,50]
+var[m1,m2,m3,m4,m5]=mark
+console.log(m1)
+console.log(m2)
+console.log(m3)
+console.log(m4)
+console.log(m5)
+var a=10
+var b=20
+var sum=a+b;
+console.log("a+b=",sum)
+console.log(a,"+",b,"=",sum)
+console.log("s(a)+a(b)=s(sum)")
+
+
+
